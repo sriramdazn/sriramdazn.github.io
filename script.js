5032e33d5461a611a15e4853cbea67a95abf243e
sriramdazn <sriram.srinivasan@dazn.com>
2024-07-12 13:46:02 +0530

    test

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
   try{
+//__webpack_require__(/*! fastestsmallesttextencoderdecoder */ "./node_modules/fastestsmallesttextencoderdecoder/EncoderDecoderTogether.min.js");
 document.body.innerHTML = "test2 ";
 
   }
